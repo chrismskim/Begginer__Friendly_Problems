e416069c3508222df599ba411124d5b1b9b2df84
chris <chrismskim@naver.com>
2024-09-25 21:51:53 +0900

    feat: print 함수의 포맷 변경- fouund at, pos를 found at positon {pos}로 변경해 가독성을 높임

--- PYTHON/2ndOct_Priyanshu-792.py
+++ PYTHON/2ndOct_Priyanshu-792.py
@@ -1,24 +1,38 @@
-pos=-1
-def search(arr,n):
-    i=0
-    while i<len(arr):
-        if arr[i]==n:
-            globals()['pos']=i
+# Initialize global variable to store the position of the found element
+pos = -1
+
+# Function to search for a value in the array
+def search(arr, n):
+    i = 0
+    while i < len(arr):
+        if arr[i] == n:
+            globals()['pos'] = i  # Update global variable 'pos' with the index of the found element
             print("Found here")
             return True
-        i=i+1
-    return False        
+        i = i + 1
+    return False
 
+# Import array module
 from array import *
-arr=array('i',[])
-n=int(input("Enter the length of the array"))
-print("Enter datas")
+
+# Create an empty integer array
+arr = array('i', [])
+
+# Input: Length of the array
+n = int(input("Enter the length of the array: "))
+
+# Input: Elements of the array
+print("Enter the elements:")
 for i in range(n):
-    x=int(input())
+    x = int(input())
     arr.append(x)
-print("Enter the value need to be searched")   
-s=int(input())
-if search(arr,s):
-    print("Found at ",pos)
+
+# Input: Value to be searched
+s = int(input("Enter the value to be searched: "))
+
+# Search for the value in the array
+if search(arr, s):
+    print(f"Found at position {pos}")
 else:
-    print("not Found")    
+    print("Not found")
+
